6c09ea81a8d783c3393b7ec5ff33d4c6c4c7d043
Denis <Denis@email.com>
2025-06-24 17:49:21 +1000

    добвил еще тестов в BookDAOTest

--- src/test/java/org/example/dao/BookDAOTest.java
+++ src/test/java/org/example/dao/BookDAOTest.java
@@ -72,6 +72,97 @@ public class BookDAOTest {
         }
     }
 
+    @Test
+    void testGetTitleBooks() {
+        bookDAO.addBook(new Book("Test1", 1234));
+        bookDAO.addBook(new Book("Test2", 1321));
+
+        java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(out));
+
+        bookDAO.getTitleBooks();
+
+        String output = out.toString();
+
+        assertAll(
+                () -> assertTrue(output.contains("Test1")),
+                () -> assertTrue(output.contains("Test2"))
+        );
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    void testGetTitleAndYearBooks() {
+        bookDAO.addBook(new Book("Test11", 1234));
+        //bookDAO.addBook(new Book("Test12", 1321));
+
+        java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(out));
+
+        bookDAO.getTitleAndYearBooks();
+
+        String output = out.toString();
+
+        assertTrue(output.contains("Test11"));
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    void testUpdate1Book() throws SQLException {
+        int id;
+        try (Connection conn = bookDAO.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(
+                     "INSERT INTO books (title, year) VALUES ('Test1', 1234)" +
+                             " RETURNING id")) {
+            ResultSet rs = pstmt.executeQuery();
+            assertTrue(rs.next());
+            id = rs.getInt("id");
+        }
+
+        bookDAO.updateBook(id, "Test12");
+
+        try (Connection conn = bookDAO.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(
+                     "SELECT title FROM books WHERE id = ?")) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            assertTrue(rs.next());
+            assertEquals("Test12", rs.getString("title"));
+        }
+    }
+
+    @Test
+    void testUpdate2Book() throws SQLException {
+        int id;
+        try (Connection conn = bookDAO.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(
+                     "INSERT INTO books (title, year) VALUES ('Test2', 2134)" +
+                             " RETURNING id")) {
+            ResultSet rs = pstmt.executeQuery();
+            assertTrue(rs.next());
+            id = rs.getInt("id");
+        }
+
+        bookDAO.updateBook(id, "Test3", "1234");
+
+        try (Connection conn = bookDAO.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(
+                     "SELECT title, year FROM books WHERE id = ?")) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            assertTrue(rs.next());
+            //assertEquals("Test3", rs.getString("title"));
+            //assertEquals("1234", rs.getString("year"));
+
+            assertAll(
+                    () -> assertEquals("Test3", rs.getString("title")),
+                    () -> assertEquals("1234", rs.getString("year"))
+            );
+        }
+    }
+
     @AfterAll
     public static void stop() {
         postgres.stop();
